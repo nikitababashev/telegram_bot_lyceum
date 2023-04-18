75f324713e5f5060cac09f9456f945ec7df1f5e1
nikita_babashev <babashev2006@mail.ru>
2023-04-18 21:58:19 +0300

    final profile

--- main.py
+++ main.py
@@ -35,8 +35,10 @@ async def start(update, context):
                                         f" {context.user_data['name']}!\n"
                                         f"С тобой по-прежнему дружелюбный Бот-Кеша!\n"
                                         f"С чего хочешь начать, практика или теория?\n"
-                                        f"*не забывай, что ты можешь остановить"
-                                        f" бота в любое время с помощью команды - /sdialog*",
+                                        f"*не забывай про имеющиеся команды:\n"
+                                        f"/sdialog - остановить бота в любое время"
+                                        f" и начать диалог заново, написав /start\n"
+                                        f"/profile - посмотреть свой профиль*",
                                         reply_markup=markup)
         return 'distribution'
     else:
@@ -51,7 +53,16 @@ async def first_response(update, context):
     if context.user_data['name'] == update.message.chat.first_name:
         reply_keyboard = [['практика', 'теория']]
         markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+
+        cur.execute(f"""INSERT INTO statistics(id_users, really_name)
+                        VALUES({context.user_data['id_user']}, '{context.user_data['name']}')""")
+        con.commit()
+
         await update.message.reply_text(f"Приятно познакомиться, {context.user_data['name']}!\n"
+                                        f"Чтобы я мог запомнить тебя, вызови команду "
+                                        f"/create_profile_user, эта функция создаcт твой личный"
+                                        f" профиль.\n"
+                                        f"Посмотреть его можно через команду - /profile\n"
                                         f"Меня зовут Бот-Кеша.\n"
                                         f"Хочешь ли ты попробовать попрактиковаться в решение задач"
                                         f" из огэ или егэ по программированию?\n"
@@ -75,10 +86,20 @@ async def distribution_fr(update, context):
         markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
         context.user_data['fake_name'] = context.user_data['name']
         context.user_data['name'] = update.message.chat.first_name
+
+        cur.execute(f"""INSERT INTO statistics(id_users, really_name, fake_nickname)
+                        VALUES({context.user_data['id_user']}, '{context.user_data['name']},
+                         '{context.user_data['fake_name']}')""")
+        con.commit()
+
         await update.message.reply_text(f"Мы - боты, развитые объекты, нас не так-то просто "
                                         f"обмануть!\n"
                                         f"Приятно познакомиться,"
                                         f" {context.user_data['name']}!\n"
+                                        f"Чтобы я мог запомнить тебя, вызови команду "
+                                        f"/create_profile_user, эта функция создаcт твой личный"
+                                        f" профиль.\n"
+                                        f"Посмотреть его можно через команду - /profile\n"
                                         f"Меня зовут Бот-Кеша.\n"
                                         f"Хочешь ли ты попробовать попрактиковаться в решение задач"
                                         f" из огэ или егэ по программированию?\n"
@@ -90,8 +111,17 @@ async def distribution_fr(update, context):
     elif answer == 'нет' or answer == 'не верно':
         reply_keyboard = [['практика', 'теория']]
         markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+
+        cur.execute(f"""INSERT INTO statistics(id_users, really_name)
+                        VALUES({context.user_data['id_user']}, '{context.user_data['name']}')""")
+        con.commit()
+
         await update.message.reply_text(f"Прошу прощения, мы - боты, еще не до конца развиты!\n"
                                         f"Приятно познакомиться, {context.user_data['name']}!\n"
+                                        f"Чтобы я мог запомнить тебя, вызови команду "
+                                        f"/create_profile_user, эта функция создаcт твой личный"
+                                        f" профиль.\n"
+                                        f"Посмотреть его можно через команду - /profile\n"
                                         f"Меня зовут Бот-Кеша.\n"
                                         f"Хочешь ли ты попробовать попрактиковаться в решение задач"
                                         f" из огэ или егэ по программированию?\n"
@@ -108,11 +138,13 @@ async def distribution_fr(update, context):
 
 async def distribution(update, context):
     answer = update.message.text.lower()
-    if update.message.chat.id not in check_registration_users:
-        cur.execute(f"""INSERT INTO statistics(id_users, really_name)
-                        VALUES({context.user_data['id_user']}, '{context.user_data['name']}')""")
-        con.commit()
+
     if answer == 'практика':
+
+        cur.execute(f"""UPDATE statistics SET count_practice = count_practice + 1
+                    WHERE id_users = {update.message.chat.id}""")
+        con.commit()
+
         reply_keyboard = [['ОГЭ', 'ЕГЭ']]
         markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False,
                                      input_field_placeholder="любим гусей!")
@@ -127,9 +159,15 @@ async def distribution(update, context):
                                             reply_markup=markup)
         return 'distribution_oge_or_ege'
     if answer == 'теория':
+
+        cur.execute(f"""UPDATE statistics SET count_theory = count_theory + 1
+                    WHERE id_users = {update.message.chat.id}""")
+        con.commit()
+
         await update.message.reply_text(f"Теория не менее важна, чем практика!\n"
                                         f"Ознакомиться с теорией можешь на нашем сайте:\n"
-                                        f"      https://lyceum.yandex.ru/courses/766/groups/5718")
+                                        f"      https://lyceum.yandex.ru/courses/766/groups/5718",
+                                        reply_markup=ReplyKeyboardRemove())
 
 
 async def distribution_oge_or_ege(update, context):
@@ -194,20 +232,45 @@ async def create_profile_user(update, context):
     created_profile = cur.execute(f"""SELECT created_profile FROM statistics
                             WHERE id_users = {update.message.chat.id}""").fetchall()[0][0]
     if created_profile == 'no':
-        print('1')
         user_id = update.message.from_user.id
 
         cur.execute(f"""UPDATE statistics SET created_profile = 'yes' WHERE id_users = {user_id}""")
         con.commit()
 
         date_created = str(datetime.now()).replace('.', ' ').split()[0:-1]
-        print(date_created)
+        date_created_profile = f"{date_created[0]} {date_created[1]}"
+        cur.execute(f"""UPDATE statistics SET date_created_profile = '{date_created_profile}'
+                    WHERE id_users = {user_id}""")
+        con.commit()
+
+        await update.message.reply_text('Ваш профиль создан\n'
+                                        'Чтобы посмотреть его, напишите /profile')
+    elif created_profile == 'yes':
+        await update.message.reply_text('Вы уже создали свой профиль!\n'
+                                        'Чтобы посмотреть его, напишите /profile')
+
+async def view_profile(update, context):
+    created_profile = cur.execute(f"""SELECT created_profile FROM statistics
+                            WHERE id_users = {update.message.chat.id}""").fetchall()[0][0]
+
+    if created_profile == 'yes':
+        count_practice = cur.execute(f"""SELECT count_practice FROM statistics
+                            WHERE id_users = {update.message.chat.id}""").fetchall()[0][0]
+        count_theory = cur.execute(f"""SELECT count_theory FROM statistics
+                            WHERE id_users = {update.message.chat.id}""").fetchall()[0][0]
+        if count_practice != count_theory:
+            if count_practice > count_theory:
+                cur.execute(f"""UPDATE statistics SET favorite_activity = 'практика'
+                            WHERE id_users = {update.message.chat.id}""")
+            elif count_practice < count_theory:
+                cur.execute(f"""UPDATE statistics SET favorite_activity = 'теория'
+                            WHERE id_users = {update.message.chat.id}""")
+            con.commit()
 
         user_photo = await telegram.Bot(token='6036045502:AAEN6Wb7h18Kfle3YDfropM7ZqIawZhH10c'). \
-            getUserProfilePhotos(user_id=user_id)
+            getUserProfilePhotos(user_id=update.message.chat.id)
         user_photo = user_photo.photos[0][-1].file_id
         with Image.open('images/temp.jpg') as im:
-
             new_im = Image.new('RGB', (250, 250), (255, 255, 255))
             new_im.paste(im, (0, 0))
             user_photo_url = await bot_t.get_file(user_photo)
@@ -216,23 +279,23 @@ async def create_profile_user(update, context):
             img = Image.open(BytesIO(response.content))
             img = img.resize((250, 250))
             new_im.paste(img, (0, 0))
-
             new_im.save('profile.jpg')
-        await update.message.reply_text('Ваш профиль создан')
-    elif created_profile == 'yes':
-        await update.message.reply_text('Вы уже создали свой профиль!\n'
-                                        'Чтобы посмотреть его, напишите /profile')
 
+        all_informations = cur.execute(f"""SELECT id_users, really_name,
+                date_created_profile, favorite_activity
+                 FROM statistics WHERE id_users = {update.message.chat.id}""").fetchall()[0]
 
-async def view_profile(update, context):
-
-    user_id = update.message.from_user.id
-
-    image_file = open('profile.jpg', 'rb')
-    bot.send_photo(chat_id=update.message.chat_id,
-                   photo=image_file,
-                   caption=f"Имя: Никита\nID: {user_id}\n")
-    image_file.close()
+        image_file = open('profile.jpg', 'rb')
+        bot.send_photo(chat_id=update.message.chat_id,
+                       photo=image_file,
+                       caption=f"Имя: {all_informations[1]}\n"
+                               f"ID: {all_informations[0]}\n"
+                               f"Дата создания: {all_informations[2]}\n"
+                               f"Любимая деятельность: {all_informations[3]}")
+        image_file.close()
+    elif created_profile == 'no':
+        await update.message.reply_text('Вы еще не создали профиль!\n'
+                                        'Чтобы его создать, напишите /create_profile_user')
 
 
 async def sdialog(update, context):
